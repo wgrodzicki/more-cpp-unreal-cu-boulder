84f12c17b01acbc3357764e1bb1e7209ced09a1e
Wojciech Grodzicki <wojciech_grodzicki@outlook.com>
2025-03-14 17:50:57 +0100

    Added simple jumping logic.

--- Source/CourseProject2/CharacterPlayerController.cpp
+++ Source/CourseProject2/CharacterPlayerController.cpp
@@ -5,9 +5,51 @@
 
 #include "CharacterPawn.h"
 
+bool bIsJumping = false;
+bool bIsFalling = false;
+float StartingJumpHeight = 0.0f;
+float CurrentJumpHeight = 0.0f;
+
 void ACharacterPlayerController::PlayerTick(float DeltaTime)
 {
     Super::PlayerTick(DeltaTime);
+    
+    ACharacterPawn* CharacterPawn = (ACharacterPawn*)GetPawn();
+    FVector CharacterLocation = CharacterPawn->GetActorLocation();
+
+    // Falling logic
+    if (bIsFalling)
+    {
+        if (CurrentJumpHeight <= StartingJumpHeight)
+        {
+            CharacterLocation.Z = StartingJumpHeight;
+            CharacterPawn->SetLocation(CharacterLocation);
+            bIsFalling = false;
+        }
+        else
+        {
+            CurrentJumpHeight -= JumpSpeed;
+            CharacterLocation.Z = CurrentJumpHeight;
+            CharacterPawn->SetLocation(CharacterLocation);
+        }
+    }
+    // Jumping logic
+    else if (bIsJumping)
+    {
+        if (CurrentJumpHeight > JumpHeight)
+        {
+            bIsFalling = true;
+            bIsJumping = false;
+        }
+        else
+        {
+            CurrentJumpHeight += JumpSpeed;
+            CharacterLocation.Z = CurrentJumpHeight;
+            CharacterPawn->SetLocation(CharacterLocation);
+        }
+    }
+
+    // Moving logic
 
     // Convert mouse movement to in-world coordinates
     FVector WorldLocation;
@@ -18,13 +60,21 @@ void ACharacterPlayerController::PlayerTick(float DeltaTime)
     WorldLocation.X = 0.0f;
 
     // Move the pawn
-    ACharacterPawn* CharacterPawn = (ACharacterPawn*)GetPawn();
 
     if (CharacterPawn != nullptr)
     {   
         // Workaround for 3D position
         WorldLocation.Y *= 10;
-        WorldLocation.Z *= 10;
+
+        // Disable mouse up/down movement when jumping or falling
+        if (bIsJumping || bIsFalling)
+        {
+            WorldLocation.Z = CharacterLocation.Z;
+        }
+        else
+        {
+            WorldLocation.Z *= 10;
+        }
 
         CharacterPawn->SetLocation(WorldLocation);
     }
@@ -35,14 +85,28 @@ void ACharacterPlayerController::SetupInputComponent()
     Super::SetupInputComponent();
 
     check(InputComponent != nullptr);
+    
     InputComponent->BindAction("DestroyCharacter",
         EInputEvent::IE_Pressed, this,
         &ACharacterPlayerController::DestroyCharacterPawn);
+
+    InputComponent->BindAction("Jump",
+        EInputEvent::IE_Pressed, this,
+        &ACharacterPlayerController::Jump);
 }
 
 void ACharacterPlayerController::DestroyCharacterPawn()
 {
     ACharacterPawn* characterPawn = (ACharacterPawn*)GetPawn();
     check(characterPawn != nullptr);
-    characterPawn->Destroy();
+    //characterPawn->Destroy();
+}
+
+void ACharacterPlayerController::Jump()
+{
+    ACharacterPawn* characterPawn = (ACharacterPawn*)GetPawn();
+    FVector characterLocation = characterPawn->GetActorLocation();
+    StartingJumpHeight = characterLocation.Z;
+    CurrentJumpHeight = StartingJumpHeight;
+    bIsJumping = true;
 }
--- Source/CourseProject2/CharacterPlayerController.h
+++ Source/CourseProject2/CharacterPlayerController.h
@@ -13,9 +13,14 @@ UCLASS()
 class COURSEPROJECT2_API ACharacterPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+private:
+    const float JumpHeight = 50.0f;
+    const float JumpSpeed = 5.0f;
 	
 public:
     virtual void PlayerTick(float DeltaTime) override;
     virtual void SetupInputComponent() override;
     void DestroyCharacterPawn();
+    void Jump();
 };
